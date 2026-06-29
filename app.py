ca0962e74f0d54c3f78591d3dec1cac2b618ed23
yadusaheb5-oss <yadusaheb5@gmail.com>
2026-06-29 09:58:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from datetime import datetime
 from zoneinfo import ZoneInfo
